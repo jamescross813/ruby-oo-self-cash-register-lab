66b874dcabb07b18fc018fa701ef99d3d9be8506
jamescross <crossjames813@gmail.com>
2021-01-31 16:15:40 -0600

    commit tree cbfd905d94ce0dc58b3819f82478a469bda76d88

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,35 @@
+class CashRegister
+
+attr_accessor :cash_register, :discount, :total, :items, :last_item
+
+    def initialize(num = 1, total = 0, items = [])
+        @total = total
+        @discount = num
+        @items = items
+    end
+
+   def add_item(title, price, quantity=1)
+    @last_item = [price*quantity]
+    @items += [title] * quantity 
+    @add_item = {title=> price}
+        @total = self.total += (price*quantity)
+        
+   end
+   
+   def apply_discount
+    if self.discount > 1    
+        @discounted =  self.total / 100 * self.discount
+        @total = @total - @discounted
+        "After the discount, the total comes to $#{@total}."
+    else 
+        "There is no discount to apply."
+    end
+end
+
+    def void_last_transaction
+    @total = self.total - self.last_item.pop
+     
+    end
+
+end
 
